a8b2aef540b2dfe9b28f8b677df58767fe54b553
Наталія Ступніченко <130250304+NataliiaBumbu@users.noreply.github.com>
2025-03-20 12:21:19 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Виправляє маршрутизацію у Vercel
-})
+  base: "/", // Додає правильні шляхи для Vercel
+});
